639c793366503e39e948d82cbe14594f00a1e630
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-04-15 16:07:03 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/', // sangat penting agar tidak hardcoded http://
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
