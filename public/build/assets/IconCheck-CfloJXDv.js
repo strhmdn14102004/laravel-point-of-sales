3ab7034827da3030d5be890dd5842b21e7542212
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-04-15 15:31:57 +0700

    Add Vite build files

---
+++ public/build/assets/IconCheck-CfloJXDv.js
@@ -0,0 +1,6 @@
+import{c as e}from"./DashboardLayout-DVYg_uCQ.js";/**
+ * @license @tabler/icons-react v3.5.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o=e("outline","check","IconCheck",[["path",{d:"M5 12l5 5l10 -10",key:"svg-0"}]]);export{o as I};
---
+++ public/build/assets/IconStack-CbwzsOGt.js
@@ -0,0 +1,11 @@
+import{c as a}from"./DashboardLayout-DVYg_uCQ.js";/**
+ * @license @tabler/icons-react v3.5.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var t=a("outline","barcode","IconBarcode",[["path",{d:"M4 7v-1a2 2 0 0 1 2 -2h2",key:"svg-0"}],["path",{d:"M4 17v1a2 2 0 0 0 2 2h2",key:"svg-1"}],["path",{d:"M16 4h2a2 2 0 0 1 2 2v1",key:"svg-2"}],["path",{d:"M16 20h2a2 2 0 0 0 2 -2v-1",key:"svg-3"}],["path",{d:"M5 11h1v2h-1z",key:"svg-4"}],["path",{d:"M10 11l0 2",key:"svg-5"}],["path",{d:"M14 11h1v2h-1z",key:"svg-6"}],["path",{d:"M19 11l0 2",key:"svg-7"}]]);/**
+ * @license @tabler/icons-react v3.5.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var h=a("outline","stack","IconStack",[["path",{d:"M12 6l-8 4l8 4l8 -4l-8 -4",key:"svg-0"}],["path",{d:"M4 14l8 4l8 -4",key:"svg-1"}]]);export{t as I,h as a};
